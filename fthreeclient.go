6a8b7180576dbc7f086342bf50a75c6e34b8599e
supali <supali.kashyap@ericsson.com>
2021-12-29 19:48:25 +0530

    call to server

--- fthreeclient.go
+++ fthreeclient.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
 
 func main() {
 	fmt.Println("Hello, world.")
+	//httpMethod: = "GET"
+	url := "http://localhost:8080/v1/organisation/accounts/ad27e265-9605-4b4b-a0e5-3003ea9cc4dc"
+
+	client := http.Client{}
+
+	res, err := client.Get(url)
+
+	if err != nil{
+		panic(err)
+	}
+	fmt.Println(res.StatusCode)
+	defer res.Body.Close()
+	bytes, err := ioutil.ReadAll(res.Body)
+	if err != nil{
+		panic(err)
+	}
+	fmt.Println(string(bytes))
 }
